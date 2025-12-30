5ac80059469fa7d0c24d3f4c33d65632b72e520f
mE0w <romain.panno@epitech.eu>
2025-12-30 15:08:55 +0100

    Update Footer.tsx (github link)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
 
           <div className="flex items-center gap-4">
             <a
-              href="https://github.com"
+              href="https://github.com/romainpanno/shiftconvert"
               target="_blank"
               rel="noopener noreferrer"
               className="text-gray-500 hover:text-gray-700 transition-colors"
