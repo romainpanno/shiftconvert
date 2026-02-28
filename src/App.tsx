e253675ba9dd4960d586858dc10329f52c18778c
me0w <romain.panno@epitech.eu>
2026-02-28 12:02:20 +0100

    Fix vercel analytics / speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
 import { Convert } from './pages/Convert';
@@ -22,6 +24,8 @@ function App() {
     <LanguageProvider>
       <BrowserRouter>
         <ScrollToTop />
+        <Analytics />
+        <SpeedInsights />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
